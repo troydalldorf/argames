e2eea2b27b3fcb5bbaecf1eb2500dd31280bcfa6
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-02 05:57:19 -0600

    i2c display

--- tennis/tennis.ino
+++ tennis/tennis.ino
@@ -1,6 +1,19 @@
-#include "HT16K33.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-HT16K33 seg(0x70);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int pinArray[] = {2, 3, 4, 5, 6, 7, 8};
 int p1Pin = 9;
@@ -17,10 +30,13 @@ int currentT = millis();
 int currentTimer = baseTimer;
   
 void setup(){
-  seg.begin();
-  Wire.setClock(100000);
-  seg.displayOn();
-  seg.setDigits(4);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+
   for (int count=0;count<=6;count++) {
     pinMode(pinArray[count], OUTPUT);
   }
@@ -86,8 +102,21 @@ void clear()
 
 void writeScore()
 {
-    seg.displayTime(p1Score, p2Score, true, false);  // do not display leading zero.
-    seg.displayColon(1);
+  display.clearDisplay();
+  display.setTextSize(4);
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("  TENNIS");
+
+  display.setTextSize(4);
+  display.setCursor(0, 28); 
+  display.print(p1Score);
+  display.setCursor(72, 28);
+  display.println(p2Score);
+  display.display();
+
+  display.display();
 }
 
 void lose(int player) {
